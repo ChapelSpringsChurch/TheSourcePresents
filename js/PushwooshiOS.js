9c5fc28654037dfe222ba93131cd481a4505aa9b
chapelspringschurch <awest@chapelsprings.org>
2015-03-21 20:59:01 -0400

    Phone Gap

---
+++ js/PushwooshiOS.js
@@ -0,0 +1,29 @@
+function initPushwoosh()
+{
+    var pushNotification = window.plugins.pushNotification;
+ 
+    //set push notifications handler
+    document.addEventListener('push-notification', function(event) {
+        var title = event.notification.title;
+        var userData = event.notification.userdata;
+                                 
+        if(typeof(userData) != "undefined") {
+            console.warn('user data: ' + JSON.stringify(userData));
+        }
+                                     
+        alert(title);
+    });
+ 
+    //initialize Pushwoosh with projectid: "116332121444", pw_appid : "A93FE-3C952". This will trigger all pending push notifications on start.
+    pushNotification.onDeviceReady({ projectid: "116332121444", pw_appid : "A93FE-3C952" });
+ 
+    //register for pushes
+    pushNotification.registerDevice(
+        function(status) {
+            var pushToken = status;
+            console.warn('push token: ' + pushToken);
+        },
+        function(status) {
+            console.warn(JSON.stringify(['failed to register ', status]));
+        }
+    );
